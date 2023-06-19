fd90553d22635c73377de8a506348694c02fd8d1
valenbenitez <benitezvalentin046@gmail.com>
2023-06-19 18:53:54 -0300

    first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Routine from './components/Routine/Routine'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tiempo, setTiempo] = useState({ minutos: 0, segundos: 0 })
+  const [inicio, setInicio] = useState(false)
+
+  useEffect(() => {
+    let interval
+
+    if (inicio) {
+      interval = setInterval(() => {
+        setTiempo(prevTiempo => {
+          let nuevosSegundos = prevTiempo.segundos + 1
+          let nuevosMinutos = prevTiempo.minutos
+
+          if (nuevosSegundos === 60) {
+            nuevosSegundos = 0
+            nuevosMinutos += 1
+          }
+
+          return { minutos: nuevosMinutos, segundos: nuevosSegundos }
+        })
+      }, 1000) // 1000 ms = 1 segundo
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [inicio])
+
+  const handleChangeMinutos = event => {
+    const nuevosMinutos = parseInt(event.target.value, 10)
+    setTiempo({ minutos: nuevosMinutos, segundos: 0 })
+  }
+
+  const handleStop = () => {
+    setTiempo({ minutos: 0, segundos: 0 })
+    setInicio(false)
+  }
+
+  const handleStart = () => {
+    setInicio(true)
+  }
 
   return (
     <div
@@ -22,7 +63,15 @@ function App() {
     >
       <h1>Easy GYM</h1>
       <p className="read-the-docs">Chino volve a jugar al futbol</p>
-      <div style={{ width: '60%' }}>
+      <h2>
+        {tiempo.minutos}:{tiempo.segundos}
+      </h2>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button onClick={handleStart}>Iniciar</Button>
+        <Button onClick={handleStop}>Reiniciar</Button>
+      </ButtonGroup>
+      <br />
+      <div style={{ width: '80%' }}>
         <Routine />
       </div>
     </div>
--- src/components/Routine/Routine.jsx
+++ src/components/Routine/Routine.jsx
@@ -1,27 +1,38 @@
+import React, { useState } from 'react'
 import Accordion from '@mui/material/Accordion'
 import AccordionSummary from '@mui/material/AccordionSummary'
 import AccordionDetails from '@mui/material/AccordionDetails'
 import Typography from '@mui/material/Typography'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import style from './routine.module.css'
+import BasicModal from '../Modal/Modal'
 
 export default function Routine() {
+  const [open, setOpen] = useState(false)
+  const [image, setImage] = React.useState('')
+
+  function handleOpen(img) {
+    setImage(img)
+    setOpen(!open)
+  }
+
   return (
     <div
       style={{
-        width: '80%',
+        width: '100%',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column',
       }}
     >
+      <BasicModal open={open} setOpen={setOpen} img={image} />
       <Accordion>
         <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
           <Typography>Dia 1</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <img src="lunes.png" id="imagen-pequena" style={{ width: '100%', heigh: '100%' }} />
+          <img src="lunes.png" onClick={() => handleOpen('lunes.png')} style={{ width: '100%', heigh: '300%' }} />
         </AccordionDetails>
       </Accordion>
       <br />
@@ -30,7 +41,7 @@ export default function Routine() {
           <Typography>Dia 2</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <img src="martes.png" style={{ width: '100px', heigh: '80px' }} />
+          <img src="martes.png" onClick={() => handleOpen('martes.png')} style={{ width: '100%', heigh: '300%' }} />
         </AccordionDetails>
       </Accordion>
       <br />
@@ -39,7 +50,11 @@ export default function Routine() {
           <Typography>Dia 3</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <img src="miercoles.png" style={{ width: '100px', heigh: '80px' }} />
+          <img
+            src="miercoles.png"
+            onClick={() => handleOpen('miercoles.png')}
+            style={{ width: '100%', heigh: '300%' }}
+          />
         </AccordionDetails>
       </Accordion>
       <br />
@@ -48,7 +63,7 @@ export default function Routine() {
           <Typography>Dia 4</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <img src="jueves.png" style={{ width: '100px', heigh: '80px' }} />
+          <img src="jueves.png" onClick={() => handleOpen('jueves.png')} style={{ width: '100%', heigh: '300%' }} />
         </AccordionDetails>
       </Accordion>
       <br />
@@ -57,7 +72,7 @@ export default function Routine() {
           <Typography>Dia 5</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <img src="viernes.png" style={{ width: '100px', heigh: '80px' }} />
+          <img src="viernes.png" onClick={() => handleOpen('viernes.png')} style={{ width: '100%', heigh: '300%' }} />
         </AccordionDetails>
       </Accordion>
     </div>
